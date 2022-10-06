5cd5c1a277a239dc1a7dfae24af74ea7515d3ef9
pagedev <firmangel8@gmail.com>
2022-10-06 15:44:12 +0800

    add solution for secrets exercism

---
+++ secrets/lib/secrets.ex
@@ -0,0 +1,17 @@
+defmodule Secrets do
+  def secret_add(secret), do: &Kernel.+(&1, secret)
+
+  def secret_subtract(secret), do: &Kernel.-(&1, secret)
+
+  def secret_multiply(secret), do: &Kernel.*(&1, secret)
+
+  def secret_divide(secret), do: &Kernel.div(&1, secret)
+
+  def secret_and(secret), do: &Bitwise.band(&1, secret)
+
+  def secret_xor(secret), do: &Bitwise.bxor(&1, secret)
+
+  def secret_combine(secret_function1, secret_function2) do
+    &(&1 |> then(secret_function1) |> then(secret_function2))
+  end
+end
