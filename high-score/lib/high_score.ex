77a92f1ea5eb797a6369fb2e9fb5e97e53bc5a6a
pagedev <firmangel8@gmail.com>
2022-10-13 17:24:04 +0800

    add solution for high-score

---
+++ high-score/lib/high_score.ex
@@ -0,0 +1,29 @@
+defmodule HighScore do
+  @initial_score 0
+
+  def new() do
+    %{}
+  end
+
+  # analyzer test
+
+  def add_player(scores, name, score \\ 0) do
+    Map.put(scores, name, score)
+  end
+
+  def remove_player(scores, name) do
+    Map.delete(scores, name)
+  end
+
+  def reset_score(scores, name) do
+    Map.put(scores, name, @initial_score)
+  end
+
+  def update_score(scores, name, score) do
+    Map.update(scores, name, score, &(&1 + score))
+  end
+
+  def get_players(scores) do
+    Map.keys(scores)
+  end
+end
